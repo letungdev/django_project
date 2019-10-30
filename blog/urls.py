bd1d4911b2765da311d7d3a32237ce062c90343b
tung <le.tung.dev@gmail.com>
2019-10-30 17:11:17 +0700

    custom homepage

---
+++ blog/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = 'mysite'
+
+urlpatterns = [
+    path('about_us/',views.about_us, name = "about-us"),
+    path('',views.homepage, name = "home-page"),
+    path('home/',views.homepage, name = "home-page"),
+    path('blogs/',views.blogs, name = "blog-page"),
+    path('contact_us/',views.contact_us, name = "contact-page"),
+    path('course_detail/',views.course_detail, name = "details-page"),
+    path('course/',views.course_detail, name = "details-page"),
+    path('investories/',views.invest, name = "invest-page"),
+    path('students/',views.students, name = "students-page"),
+
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.shortcuts import redirect
 
-# Create your views here.
+
+
+	
+def about_us(request):
+	return render(request, "about_us.html")	
+def homepage(request):
+	return render(request, "home.html")
+def blogs(request):
+	return render(request, "blog.html")
+def contact_us(request):
+	return render(request, "contact.html")
+def course_detail(request):
+	return render(request, "course_details.html")
+def course(request):
+	return render(request, "course.html")
+def students(request):
+	return render(request, "students.html")
+def invest(request):
+	return render(request, "invest.html")
