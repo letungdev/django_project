becb36e7bcbbe62037ec8b2b3c5ceed94e1f0317
tung <le.tung.dev@gmail.com>
2019-11-13 17:55:18 +0700

    edit templates

--- blog/urls.py
+++ blog/urls.py
@@ -4,16 +4,16 @@ from . import views
 app_name = 'mysite'
 
 urlpatterns = [
-    path('about_us/',views.about_us, name = "about-us"),
+    path('ve-dien-gia/',views.about_us, name = "about-us"),
     path('',views.homepage, name = "home-page"),
-    path('home/',views.homepage, name = "home-page"),
-    path('blogs/',views.blogs, name = "blog-page"),
-    path('contact_us/',views.contact_us, name = "contact-page"),
-    path('course_detail/1',views.course_detail, name = "details-page"),
-    path('course_detail/2',views.course_detail2, name = "details-page"),
-    path('course/',views.course_detail, name = "details-page"),
-    path('investories/',views.invest, name = "invest-page"),
-    path('students/',views.students, name = "students-page"),
+    path('trang-chu/',views.homepage, name = "home-page"),
+    path('dien-dan/',views.blogs, name = "blog-page"),
+    path('lien-he/',views.contact_us, name = "contact-page"),
+    path('chi-tiet-khoa-hoc/1',views.course_detail, name = "details-page"),
+    path('chi-tiet-khoa-hoc/2',views.course_detail2, name = "details-page"),
+    path('khoa-hoc/',views.course_detail, name = "details-page"),
+    path('dau-tu/',views.invest, name = "invest-page"),
+    path('hoc-vien/',views.students, name = "students-page"),
 
 
 ]
